58d67821645979500e30d424251ad17a351dbabb
ChloeArd <chloe.ardoise@gmail.com>
2022-04-07 14:29:59 +0200

    twitter

--- react/index.js
+++ react/index.js
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./index.scss";
 import {Home} from "./pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {Header} from "./components/Header/Header";
